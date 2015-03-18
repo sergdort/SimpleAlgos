3c13a9a3699e916de75db24d1b3e591f28645f18
Sergey Shulga <sergdort@gmail.com>
2015-03-18 13:44:13 +0200

    Initial Commit

---
+++ SimpleAlgos/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleAlgos
+//
+//  Created by Sergey Shulga on 3/18/15.
+//  Copyright (c) 2015 Sergey Shulga. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
